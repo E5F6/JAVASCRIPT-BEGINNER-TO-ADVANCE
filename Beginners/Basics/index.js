17c4baba76d5d7fd005a8923c0907a5007f8683b
Priyanxhuu <priyanxhuchahar@gmail.com>
2024-03-04 15:45:11 +0530

    .

---
+++ Beginners/Basics/index.js
@@ -0,0 +1,124 @@
+// console.log("Hello World"); //console is a object with multiple methods
+
+// Print statement for javascript this print the output in console only
+
+// console.warn("This is a warning");
+
+// Warning statement for javascript this print the warning in console
+
+// There are still more methods in console object use them and see outputs
+
+// alert("Hello This is A Alerts");
+
+// Alert statement for web only works in browsers this is like a popup
+
+// prompt("Enter your Name");
+
+// Prompt is used for taking input from user in a popup like alert this also works only in browser
+
+// -----------------------------------------------------------------------
+
+//Variables
+
+// (reserved keywords) (variable name) (type of value)
+
+// reserved keywords are var , let , const
+
+// var was used in old code bases now let and const is used
+
+// let name = "Priyanshu";
+// console.log(name);
+
+// Here i assigned a value to variable and printing that value using console.log
+
+// let's assign another value
+
+// name = "Hello";
+// console.log(name);
+
+// the new assigned value is now the primary value of name so this will print new assigned value
+
+// const hello = "Priyanshu";
+// console.log(hello);
+
+// here using const variable a value is assigned and the value is printing in console
+
+// hello = "Chahar";
+// console.log(hello);
+
+// in const variable we cannot reassign a value this will throw a error
+
+// * No reserved keyword can be assigned as name of any variable
+
+// variable name cannot contain whitespaces ,- and can't start with a number they can end with a number like this is not correct 7priyanshu & this is correct priyanshu7
+
+// -----------------------------------------------------------------------
+
+// Primitive Types
+
+// 1. Number
+
+// let num = 10;
+// console.log(typeof num); // This will give type of num variable
+
+// Basic maths works as usual in javascript
+
+// let counter = 0;
+// counter ++;
+
+// console.log(counter);
+
+// a basic increment prog in js
+
+// -----------------------------------------------------------------------
+
+// Booleans
+
+// true/fale & yes/no & on/off
+
+// let isTrue = false;
+// console.log(isTrue);
+
+// falsy value in boolean
+
+// --> false;
+// --> null;
+// --> undefined;
+// --> 0 / -0;
+// --> NaN;
+// --> ' ' , "" ;
+
+// -----------------------------------------------------------------------
+
+// operators
+
+// comparision operators
+
+// --> relational operators
+
+// > Greater then
+// < less then
+// >= Greater then or equals to
+// <= Less than or equals to
+
+// console.log(10>7); // true ? fasle
+
+// -> euality operators
+
+// === strict equality(type + value)
+// !== strick non-eqality operators (type + value)
+// = lose equaity operator(value)
+// != not equality operator(value)
+
+// console.log(10 === "10"); // true ? false
+
+// -----------------------------------------------------------------------
+
+// Strings
+
+// let hello = "This is a string";
+// console.log(hello);
+
+// a string is assigned to the variable string is a group of words
+
+// -----------------------------------------------------------------------
