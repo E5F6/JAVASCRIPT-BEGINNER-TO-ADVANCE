dcfcc827044405ca11f96fb4172472908738ad5f
Priyannxhuu <priyanxhuchahar@gmail.com>
2024-03-19 07:33:24 -0700

    .

---
+++ GSAP/From And From To/script.js
@@ -0,0 +1,13 @@
+gsap.fromTo(
+    ".box2",
+    { y: 200, opacity: 0 },
+    {
+      opacity: 1,
+      y: -200,
+      duration: 3,
+      ease: "linear",
+      borderRadius: 0,
+      repeat: -1,
+      yoyo: true,
+    }
+  );
---
+++ GSAP/Tween/script.js
@@ -0,0 +1,6 @@
+gsap.to(".box", {
+    duration: 2,
+    y: 100,
+    repeat: -1,
+    yoyo: true,
+  });
